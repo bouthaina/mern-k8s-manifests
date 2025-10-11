d77bf6857a6ad11840d8eefa184149e2e4b0626c
bouthaina <bouthainabakouch@gmail.com>
2025-10-11 14:50:45 +0100

    add k8s-manifests

--- frontend-app/vite.config.js
+++ frontend-app/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
 	return {
 		plugins: [react()],
+                define: {
+                   'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+                },
 		test: {
 			globals: true,
 			environment: 'jsdom',
