2a2efb2230e76b907ca1cf0ef6823e924992cccf
bouthaina <bouthainabakouch@gmail.com>
2025-10-17 14:50:20 +0100

    create manifests

--- backend_api/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-    transform: {
-      '^.+\\.js$': 'babel-jest',  // Use babel-jest for transforming JS files
-    },
-    testEnvironment: 'node',  // Ensure you're using the Node.js environment
-  };
-  
